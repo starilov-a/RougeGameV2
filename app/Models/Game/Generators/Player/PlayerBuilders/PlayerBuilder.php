6819c2b8dcba2307a2414a98daaf3b47229677ca
starilov <anton_starilov@mail.ru>
2023-07-24 17:23:43 +0300

    Player Generator and more mini fixes

---
+++ app/Models/Game/Generators/Player/PlayerBuilders/PlayerBuilder.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Models\Game\Generators\Player\PlayerBuilders;
+
+
+class PlayerBuilder
+{
+
+}
---
+++ app/Models/Game/Generators/Player/PlayerBuilders/PlayerClassicBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Models\Game\Generators\Player\PlayerBuilders;
+
+
+class PlayerClassicBuilder extends PlayerBuilder
+{
+
+    public function reset()
+    {
+        // TODO: Implement reset() method.
+    }
+
+    public function createPlayer()
+    {
+        // TODO: Implement createPlayer() method.
+    }
+
+    public function setInRoom()
+    {
+        // TODO: Implement setInRoom() method.
+    }
+
+    public function giveItems()
+    {
+        // TODO: Implement giveItems() method.
+    }
+}
